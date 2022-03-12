2cae05c8242ddb2ddf1bbfc9bbe3814c07a1f165
Matheus Figueiredo <matheuss_figueiredo@hotmail.com>
2022-03-12 14:58:24 -0300

    Projeto criado

---
+++ backend/src/main/java/com/projetomatheus/dsmoviev2/Dsmoviev2Application.java
@@ -0,0 +1,13 @@
+package com.projetomatheus.dsmoviev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmoviev2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmoviev2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetomatheus/dsmoviev2/Dsmoviev2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomatheus.dsmoviev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmoviev2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
